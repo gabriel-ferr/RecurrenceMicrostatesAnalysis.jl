232100ac714b85df136a84688b817414e2ed32bb
Gabriel Ferreira <gabriel.vferreira@icloud.com>
2025-12-09 13:34:46 -0300

    add tests for motif shapes and recurrence expressions

--- test/distributions.jl
+++ test/distributions.jl
@@ -82,15 +82,62 @@ end
     end
 end
 
+@testset "motif shapes (with CPUCore)" begin
+    @testset "RP and CRP" begin
+        x = StateSpaceSet(rand(1000))
+        y = StateSpaceSet(rand(2000))
+
+        @test_nothing distribution(x, Rect(Standard(0.27), 2))
+        @test_nothing distribution(x, Rect(Standard(0.27), 3))
+
+        @test_nothing distribution(x, y, Rect(Standard(0.27), 2))
+        @test_nothing distribution(x, y, Rect(Standard(0.27), 3))
+    end
+    
+    @testset "SRP and CSRP" begin
+        x = rand(1, 100, 100)
+        y = rand(1, 100, 100)
+
+        @test_nothing distribution(x, Rect(Standard(0.27), (2, 2, 1, 1)))
+        @test_nothing distribution(x, Rect(Standard(0.27), (3, 3, 1, 1)))
+
+        @test_nothing distribution(x, y, Rect(Standard(0.27), (2, 2, 1, 1)))
+        @test_nothing distribution(x, y, Rect(Standard(0.27), (3, 3, 1, 1)))
+    end
+end
+
 @testset "recurrence expressions (with CPUCore)" begin
     @test Standard(0.27) isa RecurrenceExpression
     @test Standard(0.27; metric = Cityblock()) isa RecurrenceExpression
     @test Corridor(0.05, 0.27) isa RecurrenceExpression
     @test Corridor(0.05, 0.27; metric = Cityblock()) isa RecurrenceExpression
     
-    x = StateSpaceSet(rand(1000))
-    @test_nothing distribution(x, Standard(0.27), 2)
-    @test_nothing distribution(x, Standard(0.27; metric = Cityblock()), 2)
-    @test_nothing distribution(x, Corridor(0.05, 0.27), 2)
-    @test_nothing distribution(x, Corridor(0.05, 0.27; metric = Cityblock()), 2)
+    @testset "RP and CRP" begin
+        x = StateSpaceSet(rand(1000))
+        y = StateSpaceSet(rand(2000))
+        @test_nothing distribution(x, Standard(0.27), 2)
+        @test_nothing distribution(x, Standard(0.27; metric = Cityblock()), 2)
+        @test_nothing distribution(x, Corridor(0.05, 0.27), 2)
+        @test_nothing distribution(x, Corridor(0.05, 0.27; metric = Cityblock()), 2)
+
+        @test_nothing distribution(x, y, Standard(0.27), 2)
+        @test_nothing distribution(x, y, Standard(0.27; metric = Cityblock()), 2)
+        @test_nothing distribution(x, y, Corridor(0.05, 0.27), 2)
+        @test_nothing distribution(x, y, Corridor(0.05, 0.27; metric = Cityblock()), 2)
+    end
+
+    @testset "SRP and CSRP" begin
+        x = rand(1, 100, 100)
+        y = rand(1, 100, 100)
+
+        @test_nothing distribution(x, Rect(Standard(0.27), (2, 2, 1, 2)))
+        @test_nothing distribution(x, Rect(Standard(0.27; metric = Cityblock()), (2, 2, 1, 2)))
+        @test_nothing distribution(x, Rect(Corridor(0.05, 0.27), (2, 2, 1, 2)))
+        @test_nothing distribution(x, Rect(Corridor(0.05, 0.27; metric = Cityblock()), (2, 2, 1, 2)))
+
+        @test_nothing distribution(x, y, Rect(Standard(0.27), (2, 2, 1, 2)))
+        @test_nothing distribution(x, y, Rect(Standard(0.27; metric = Cityblock()), (2, 2, 1, 2)))
+        @test_nothing distribution(x, y, Rect(Corridor(0.05, 0.27), (2, 2, 1, 2)))
+        @test_nothing distribution(x, y, Rect(Corridor(0.05, 0.27; metric = Cityblock()), (2, 2, 1, 2)))
+    end
 end
